d1322564584344a2e7002f5a6c8a51da8fdb21a8
zsoh97 <zsoh97@icloud.com>
2020-01-19 13:45:53 +0800

    level-1 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        echo();
+    }
+    public static void echo(){
+        String border = "******************************************************";
+        String greeting = "Hi! My name is Alfred. I aim to please.";
+        System.out.println(border + "\n  " + greeting + "\n" + border);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")){
+            System.out.println(border + "\n  " + input + "\n" + border);
+            input = sc.nextLine();
+        }
+        System.out.println("Goodbye. I hope I was useful. See you again.");
+        sc.close();
     }
 }
