8d0c50fc274b85816a2c03ad1a224b768c5b2905
zsoh97 <zsoh97@icloud.com>
2020-01-21 22:53:11 +0800

    level 6 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,6 +26,10 @@ public class Duke {
                 String[] split;
                 switch (input) {
                     case "list":
+                        if(tasks.isEmpty()){
+                            System.out.println(border + "\n  There are currently no items in your list. \n" + border);
+                            break;
+                        }
                         System.out.println(border + "\n  " + "The items in your list are:");
                         for (int i = 1; i <= tasks.size(); i++) {
                             System.out.println("  " + (i) + ". " + tasks.get(i - 1));
@@ -35,16 +39,18 @@ public class Duke {
                     case "done":
                         Task task = tasks.get(sc.nextInt() - 1);
                         task.done();
-                        System.out.println(border + "\n  " + "Well Done! The task has been marked as done." + "\n  " + task + "\n" + border);
+                        System.out.println(border + "\n  " + "Well Done! The task has been marked as done." + "\n  "
+                                        + task + "\n" + border);
                         break;
                     case "todo":
                         description = sc.nextLine();
                         if (description.isBlank()) {
                             throw new DukeException("My apologies, description of todo is empty. :(");
                         }
-                        Todo todo = new Todo(input);
+                        Todo todo = new Todo(description);
                         tasks.add(todo);
-                        System.out.println(border + "\n  " + taskAdd + todo + "\n  " + "There are currently " + tasks.size() + " tasks in the list.\n" + border);
+                        System.out.println(border + "\n  " + taskAdd + todo + "\n  There are currently " + tasks.size()
+                                        + " tasks in the list.\n" + border);
                         break;
                     case "deadline":
                         description = sc.nextLine();
@@ -54,7 +60,8 @@ public class Duke {
                         }
                         Deadline deadline = new Deadline(split[0], split[1]);
                         tasks.add(deadline);
-                        System.out.println(border + "\n  " + taskAdd + deadline + "\n  " + "There are currently " + tasks.size() + " tasks in the list.\n" + border);
+                        System.out.println(border + "\n  " + taskAdd + deadline + "\n  There are currently "
+                                        + tasks.size() + " tasks in the list.\n" + border);
                         break;
                     case "event":
                         description = sc.nextLine();
@@ -64,7 +71,23 @@ public class Duke {
                         }
                         Event event = new Event(split[0], split[1]);
                         tasks.add(event);
-                        System.out.println(border + "\n  " + taskAdd + event + "\n  " + "There are currently " + tasks.size() + " tasks in the list.\n" + border);
+                        System.out.println(border + "\n  " + taskAdd + event + "\n  There are currently " + tasks.size()
+                                        + " tasks in the list.\n" + border);
+                        break;
+                    case "delete":
+                        description = sc.nextLine();
+                        if(description.isBlank()){
+                            throw new DukeException("My apologies, task number to be deleted not found :(");
+                        }
+                        int delete = Integer.parseInt(description.trim());
+                        if(delete > tasks.size()){
+                            throw  new DukeException("My apologies, task number to be deleted not found :(");
+                        }
+                        Task removed = tasks.get(delete-1);
+                        tasks.remove(delete-1);
+                        System.out.println(border + "\n  Noted. Task " + delete + " has been removed:\n  "
+                                        + removed + "\n  There are currently " + tasks.size() + " tasks in the list.\n"
+                                        + border);
                         break;
                     default:
                         throw new DukeException("My apologies, I do not recognise the command. :(");
