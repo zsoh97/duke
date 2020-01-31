ebb04c49e937201788fe74d0ee5e00772830c665
zsoh97 <zsoh97@icloud.com>
2020-01-31 12:33:44 +0800

    Revert "A-JUnit commit"
    This reverts commit e7bd9199cb134a3041f16ec9ee776a2037f70ea6.
    
    # Conflicts:
    #	data/duke.txt

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,24 +6,17 @@ import java.util.Locale;
 public class Task {
 
     private boolean done;
-    private String taskName, date;
+    private String taskName;
     private LocalDate lD;
     private String[] byAt;
     private Duke.Tasks taskType;
 
-//    public Task(){
-//
-//    }
 
     public Task(Duke.Tasks taskType, String taskName, String byAt){
         this.taskName = taskName;
         this.taskType = taskType;
         this.byAt = byAt.split(" ");
-        if(this.byAt.length == 2) {
-            this.lD = LocalDate.parse(this.byAt[0]);
-        }else{
-            this.date = byAt;
-        }
+        this.lD = LocalDate.parse(this.byAt[0]);
     }
 
     public Task(Duke.Tasks taskType, String taskName){
@@ -59,14 +52,10 @@ public class Task {
     }
 
     private String date(){
-        if (this.date == null) {
-            int day = this.lD.getDayOfMonth();
-            Month month = this.lD.getMonth();
-            int year = this.lD.getYear();
-            return month.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " " + day + " " + year + " " + this.byAt[1];
-        }else{
-            return this.date;
-        }
+        int day = this.lD.getDayOfMonth();
+        Month month = this.lD.getMonth();
+        int year = this.lD.getYear();
+        return month.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " " + day + " " + year;
     }
 
     public void done(){
@@ -80,9 +69,9 @@ public class Task {
                 case todo:
                     return "[T][✓] " + taskName;
                 case deadline:
-                    return "[D][✓] " + taskName + " (by: " + this.date() + ")";
+                    return "[D][✓] " + taskName + " (by: " + this.date()+ " " + this.byAt[1] + ")";
                 case event:
-                    return "[E][✓] " + taskName + " (at: " + this.date() + ")";
+                    return "[E][✓] " + taskName + " (at: " + this.date() + " " + this.byAt[1] + ")";
                 default:
                     return "[✓]" + taskName;
             }
@@ -91,9 +80,9 @@ public class Task {
                 case todo:
                     return "[T][✗] " + taskName;
                 case deadline:
-                    return "[D][✗] " + taskName + " (by: " + this.date() + ")";
+                    return "[D][✗] " + taskName + " (by: " + this.date() + " " + this.byAt[1] + ")";
                 case event:
-                    return "[E][✗] " + taskName + " (at: " + this.date() + ")";
+                    return "[E][✗] " + taskName + " (at: " + this.date() + " " + this.byAt[1] + ")";
                 default:
                     return "[✗]" + taskName;
             }
